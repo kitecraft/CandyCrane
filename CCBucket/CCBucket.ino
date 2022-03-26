8788b0a1adccf45d0acbde64084bbff99bef24d9
Kitecraft Unborn <kitecraft@gmail.com>
2022-03-26 12:26:24 -0600

    Initial Commit

---
+++ CCBucket/CCBucket.ino
@@ -0,0 +1,13 @@
+/*
+ Name:		CCBucket.ino
+ Created:	3/26/2022 12:18:42 PM
+ Author:	Kitecraft
+*/
+
+void setup() {
+
+}
+
+void loop() {
+  
+}
---
+++ CandyCrane/CandyCrane.ino
@@ -0,0 +1,13 @@
+/*
+ Name:		CandyCrane.ino
+ Created:	3/26/2022 12:17:55 PM
+ Author:	Kitecraft
+*/
+
+void setup() {
+
+}
+
+void loop() {
+  
+}
