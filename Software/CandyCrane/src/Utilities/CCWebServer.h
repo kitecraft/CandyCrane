43cc5fe4bd23c83e1d535122aa085aa71be49a8f
Kitecraft Unborn <kitecraft@gmail.com>
2022-05-05 22:44:50 -0600

    Working OTA, Webserver

--- Software/CandyCrane/src/Utilities/CCWebServer.h
+++ Software/CandyCrane/src/Utilities/CCWebServer.h
@@ -5,10 +5,14 @@
 #include <ESPmDNS.h>
 #include <uri/UriBraces.h>
 #include "../CC_Config.h"
+#include "IceFS.h"
+#include "../Crane/CraneController.h"
+
+static WebServer server(80);
+extern CraneController g_craneController;
 
 void IRAM_ATTR WebSeverThread(void*)
 {
-    static WebServer server(80);
     if (MDNS.begin(__DEVICE_NAME__)) {
         Serial.println("MDNS responder started");
     }
@@ -16,6 +20,45 @@ void IRAM_ATTR WebSeverThread(void*)
     server.on(F("/"), []() {
         server.send(200, "text/plain", "hello from esp32!");
         });
+
+    server.on(F("/reboot"), []() {
+        ESP.restart();
+        });
+
+    server.on(F("/net"), []() {
+        String webPage = IceFS_ReadFile(WEBPAGE_NETWORK);
+        webPage.replace("%{__DEVICE_NAME__}%", __DEVICE_NAME__);
+        webPage.replace("%{ERRORTEXT}%", "");
+        webPage.replace("%{NETWORKNAME}%", GetSsid());
+        server.send(200, "text/html", webPage);
+        });
+
+    server.on(F("/setupNet"), []() {
+        String webPage = IceFS_ReadFile(WEBPAGE_NETWORK);
+        if (server.arg("password") != server.arg("passwordConfirm"))
+        {
+            webPage.replace("%{ERRORTEXT}%", "Passwords did not match.<br>Changes not saved.");
+        }
+        else {
+            SetSsid(server.arg("networkName"));
+            SetSsidPassword(server.arg("password"));
+            webPage.replace("%{ERRORTEXT}%", "Changes have been saved.");
+        }
+        webPage.replace("%{NETWORKNAME}%", GetSsid());
+        server.send(200, "text/html", webPage);
+        });
+
+
+    server.on(F("/commands/tower/Home"), [&]() {
+        server.send(200, "text/plain", "Homeing");
+        });
+
+    server.on(F("/commands/tower/Stop"), [&]() {
+        g_craneController.StopTowerMotion();
+        server.send(200, "text/plain", "Stopped Tower");
+        });
+
+
 
     server.on(UriBraces("/users/{}"), []() {
         String user = server.pathArg(0);
